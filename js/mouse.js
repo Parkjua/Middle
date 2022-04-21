633cdea0cc330f4f1277338e6d8db96eb8c8067b
Parkjua <100866119+Parkjua@users.noreply.github.com>
2022-04-22 06:17:53 +0900

    Add files via upload

---
+++ js/mouse.js
@@ -0,0 +1,45 @@
+//로드 이벤트 : 모든 태그, 이미지 등이 로드되면 실행
+window.onload = function () {
+  var mouseCursor = document.querySelector(".cursor");
+  var navLinks = document.querySelectorAll(".gnbMenu li");
+  console.log(navLinks);
+
+  navLinks.forEach(function (menu) {
+    console.log(menu);
+    menu.addEventListener("mouseover", function () {
+      console.log("over");
+      mouseCursor.classList.add("link_hover");
+      mouseCursor.style.zIndex = "30";
+    });
+    menu.addEventListener("mouseout", function () {
+      console.log("out");
+      mouseCursor.classList.remove("link_hover");
+      mouseCursor.style.zIndex = "10";
+    });
+  });
+
+  document.querySelectorAll(".text1").forEach((em) => {
+    em.addEventListener("mouseover", () => {
+      mouseCursor.classList.add("show2");
+    });
+    em.addEventListener("mouseout", () => {
+      mouseCursor.classList.remove("show2");
+    });
+  });
+
+  document.querySelectorAll(".text2").forEach((em) => {
+    em.addEventListener("mouseover", () => {
+      mouseCursor.classList.add("show3");
+    });
+    em.addEventListener("mouseout", () => {
+      mouseCursor.classList.remove("show3");
+    });
+  });
+  // 함수 선언
+  function myCursor(e) {
+    document.querySelector(".cursor").style.top = e.pageY + "px";
+    document.querySelector(".cursor").style.left = e.pageX + "px";
+  }
+
+  window.addEventListener("mousemove", myCursor);
+};
